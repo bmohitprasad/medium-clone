f3373f0e4d0852b05bc85c6d2f42051a19bb84d5
bmohitprasad <bmohitprasad@gmail.com>
2024-06-24 20:59:34 +0530

    app bar spaces

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -10,13 +10,13 @@ export const Appbar = ()=>{
 
     const authorName = blogs.length > 0 ? blogs[0].author.name : "Anonymous"
     
-    return <div className="border-b-4 border-black rounded-b-2xl bg-slate-200 flex justify-between px-10 py-4">
+    return <div className="border-b-4 border-black rounded-b-2xl bg-slate-200 flex justify-between px-4 py-4">
         <Link to={`/blogs`} className="font-mono text-lg flex flex-col justify-center cursor-pointer">
                 Medium
         </Link>
         <div className="flex justify-center">
             <Link to={`/publish`}>
-                <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center ">New +</button>
+                <button type="button" className=" mr-2 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center ">New +</button>
             </Link>
             
 
@@ -27,7 +27,7 @@ export const Appbar = ()=>{
                     sessionStorage.removeItem('authToken'); 
                   }
             }>
-                <button type="button" className="pl-4">
+                <button type="button" className="pl-4 lg:pl-2">
                 <FaSignOutAlt style={{ marginRight: '12px' }} />
                 Signout
                 </button>
