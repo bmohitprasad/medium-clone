f3aaa4d253942b0be4ac1d9ce343a08f160fdf30
B Mohit Prasad <114416951+bmohitprasad@users.noreply.github.com>
2024-09-14 21:50:17 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'bebas-neue': ['"Bebas Neue"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
