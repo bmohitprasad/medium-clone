6be114a584e5900d9df1cdc0e82693896f67be9c
B Mohit Prasad <114416951+bmohitprasad@users.noreply.github.com>
2024-09-14 21:46:40 +0530

    Update FullBlog.tsx

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -1,16 +1,48 @@
+import { Blog } from "../hooks"
+import { Appbar } from "./Appbar"
+import { Avatar, formatDateString } from "./BlogCard"
 
-export const Quote = () =>{
-    return <div className="bg-slate-200 flex justify-center flex-col h-auto">
-        <div className="flex justify-center">
-            <div className="p-4">
-                <div className="flex justify-center text-2xl font-bebas-neue">
-                  How to Use the Blogs Page
-                </div>
-                <div className="flex justify-center max-w-md text-left mt-4 mb-2 font-light">
-                Welcome to the Blogs page! Here, you can browse through a variety of blog posts from other users.
-                Click on any blog title to read the full post. If you're logged in, you can create and publish your own blogs!
-                </div>
+export const FullBlog = ({ blog }: { blog: Blog }) => {
+  const date = String(blog.date);
+
+  return (
+    <div>
+      <Appbar />
+      <div className="flex justify-center">
+        <div className="grid grid-cols-12 w-full px-10 pt-10 max-w-screen-xl">
+          <div className="col-span-12 lg:col-span-8">
+            <div className="text-5xl font-bold">{blog.title}</div>
+            <div className="text-slate-500 pt-2">
+              Published on {formatDateString(date)}
             </div>
+            <div
+              className="pt-6 text-justify leading-loose" 
+              style={{
+                paddingTop: "32px",
+              }}
+            >
+              {blog.content}
+            </div>
+          </div>
+          <div className="col-span-12 lg:col-span-4 ml-10">
+            <div className="text-slate-600 text-lg mt-10 lg:mt-0">Author</div>
+            <div className="flex w-full">
+              <div className="pr-4 flex flex-col justify-center">
+                <Avatar size="big" name={blog.author.name || "Anonymous"} />
+              </div>
+              <div>
+                <div className="text-xl font-bold">
+                  {blog.author.name || "Anonymous"}
+                </div>
+                <div className="pt-2 text-slate-500">
+                  Random catchphrase about the author's ability to grab the user's attention
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-}
+  );
+};
+
