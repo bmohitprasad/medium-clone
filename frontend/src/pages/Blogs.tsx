ad4f1878590c3f06bd7f3d902ad7d56ea669fdb5
B Mohit Prasad <114416951+bmohitprasad@users.noreply.github.com>
2024-09-14 21:47:10 +0530

    Update Blogs.tsx

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,6 +1,7 @@
 import { Appbar } from "../components/Appbar"
 import { BlogCard } from "../components/BlogCard"
 import { BlogSkeleton } from "../components/BlogSkeleton"
+import { Quote} from "../components/Quote"
 import { useBlogs } from "../hooks"
 
 export const Blogs = () => {
@@ -9,7 +10,7 @@ export const Blogs = () => {
     if (loading){
         return<div>
             <Appbar/>
-            <div className="flex justify-center ">
+            <div className="flex justify-center">
                 <div>
                     <BlogSkeleton/>
                     <BlogSkeleton/>
@@ -24,20 +25,25 @@ export const Blogs = () => {
 
     return <div>
         <Appbar/>
-        <div className="flex justify-center w-full">
-            <div>
-                {blogs.map(blog =>
-                <BlogCard
-                    key={blog.id}
-                    //@ts-ignore
-                    id = {blog.id}
-                    authorName={blog.author.name || "Anonymous"}
-                    title={blog.title}
-                    content={blog.content}
-                    publishedDate={String (blog.date)}
-                />
-                )}
+        <div className="flex justify-center w-full flex-wrap lg:flex-nowrap">
+            <div className="flex ml-4">
+                <div>
+                    {blogs.map(blog =>
+                    <BlogCard
+                        key={blog.id}
+                        //@ts-ignore
+                        id = {blog.id}
+                        authorName={blog.author.name || "Anonymous"}
+                        title={blog.title}
+                        content={blog.content}
+                        publishedDate={String (blog.date)}
+                    />
+                    )}
+                </div>
+            </div>
+            <div className="flex h-1/2 m-4 sticky top-10 bg-white border-4 border-black">
+                <Quote/>
+            </div>
         </div>
-    </div>
     </div>
 }
