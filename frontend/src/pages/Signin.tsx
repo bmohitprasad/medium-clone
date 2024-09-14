d2562da70fe7c5954505d2e50625fab2b2a510af
B Mohit Prasad <114416951+bmohitprasad@users.noreply.github.com>
2024-09-14 21:47:38 +0530

    Update Signin.tsx

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,17 +1,16 @@
-import { Quote } from "../components/Quote"
 import { Auth } from "../components/Auth"
+import { DescriptionCard } from "../components/DescriptionCard"
 
 
 export const Signin = () => {
-    return <div>
-        <div className="grid grid-cols-1 lg:grid-cols-2">
-        <div>
+    return <div className="">
+        <div className="bg-slate-200 h-full w-full flex justify-center grid grid-cols-1 lg:grid-cols-2">
+            <div className="mt-4">
                 <Auth type="signin"/>
             </div>
-            <div className="hidden lg:block">
-                <Quote/>
+            <div className="flex bg-white rounded-2xl border-8 border-gray-700 ml-4 mt-4 mb-4 mr-4 lg:mr-16">
+                <DescriptionCard/>
             </div>
-            
         </div>
         
     </div>
