241c06db258a7ea5878f40ce798dee349039ad57
bmohitprasad <bmohitprasad@gmail.com>
2025-03-16 05:04:08 +0530

    Updated 2.0

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -24,7 +24,7 @@ export const Sidebar = () => {
                         { icon: <FaHome />, label: "Home", link: "/blogs" },
                         { icon: <FaPen />, label: "Publish", link: "/publish" },
                         { icon: <FaBookOpen />, label: "My Blogs", link: "/yourblogs" },
-                        { icon: <FaCog />, label: "Settings", link: "/settings" },
+                        { icon: <FaCog />, label: "Settings", link: "/blogs" },
                         { icon: <FaSignOutAlt />, label: "Logout", link: "/home" },
                     ].map((item, index) => (
                         <a
